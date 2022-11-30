6ab91b7114f2ef96cecbbd627df6a0617e7f3a32
Sofia Sivilotti <68237440+ssivilotti@users.noreply.github.com>
2022-11-29 23:52:32 -0600

    BFS test case

--- tests/test.cc
+++ tests/test.cc
@@ -2,18 +2,34 @@
 #define CATCH_CONFIG_MAIN
 #endif
 #include "catch.hpp"
+#include "../src/flightgraph.h"
+#include "../src/Flight.h"
+#include "../src/Airport.h"
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <string>
 #include <stack>
 
+
+
 using namespace std;
 
-TEST_CASE("PUT YOUR TESTS HERE!!!", "[tag1][tag2]") {
-    // Feel free to use this to put your own test cases.
-    // Look at other test files for inspiration!
-    string expected = "I put my own test cases here.";
-    string actual = "I DON'T HAVE ANY TEST CASES :'(";
-    REQUIRE(expected == actual);
+TEST_CASE("BFS1", "[BFS]") {
+    Graph test = Graph("data/airports.txt","data/routes.txt");
+    vector<int> expected = vector<int>({2811, 2833});
+    vector<Flight> actual = test.getTraversal(2811, 2833);
+    REQUIRE(compareVects(expected, actual)==true);
+}
+
+bool compareVects(const vector<int>& v1, const vector<Flight>& v2) {
+    if (v1.size() + 1 != v2.size()) {
+        return false;
+    }
+    for (unsigned i = 0; i<v2.size(); i++) {
+        if (!(v1.at(i) == v2.at(i).getStartID()) && v1.at(i + 1) == v2.at(i).getDestinationID()) {
+            return false;
+        }
+    }
+    return true;
 }
