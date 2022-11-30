d83439c58cf23c3bf3bf4eaae0b59760bd476e94
rohanjs3 <rohanjs3@illinois.edu>
2022-11-29 21:49:34 -0600

    BFS

---
+++ src/BFS.cpp
@@ -0,0 +1,36 @@
+#include "BFS.h"
+#include <unordered_map>
+#include "Airport.h"
+#include "Flight.h"
+#include <string>
+#include <vector>
+#include <queue>
+
+std::vector<Flight> getTraversal(unordered_map<int, Airport> airports_, unordered_map<int, vector<Flight>> flights_, Airport start, Airport end)
+{
+    std::vector<int> visited;
+    visited.resize(airports_.size());
+    std::queue<Airport> q;
+    visited[start.getID()] = 1;
+    q.push(start);
+    Airport v;
+    std::vector<Flight> toReturn;
+    while (!q.empty())
+    {
+        v = q.pop();
+        for (Flight f : flights_[v.getID()])
+        {
+            // for all flights at airport v...
+            int id = f.getDestinationID();
+            Airport dest = airports_[f.getDestinationID()];
+            if (visited[id] == 0)
+            {
+                // unexplored node
+                visited[id] == 1;
+                toReturn.push_back(f);
+                q.push(dest);
+            }
+        }
+    }
+    return toReturn;
+}
